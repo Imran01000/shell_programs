540d500935b3fd32cf00276f5c32dbcdc50db265
Imran01000 <imshaik01000@gmail.com>
2020-02-19 12:57:34 +0530

    by floating point

--- elif_programs/number_arithmetic.sh
+++ elif_programs/number_arithmetic.sh
@@ -1,35 +1,35 @@
 read -p "Enter the value of a:" a;
 read -p "Enter the value of b:" b;
 read -p "Enter the value of c:" c;
-res1=$(($a+$b*$c)); 
-res2=$(($c+$a/$b));
-res3=$(($a%$b+$c));
-res4=$(($a*$b+$c));
+res1=`echo "scale=2; $a+$b*$c" | bc -l`; 
+res2=`echo "scale=2; $c+$a/$b" | bc -l`;
+res3=`echo "scale=2; $a%$b+$c" | bc -l`;
+res4=`echo "scale=2; $a*$b+$c" | bc -l`;
 echo "$res1,$res2,$res3,$res4"
 max=$res1;
 min=$res1;
-if ((res1<res2))
+if (( `echo "$res1<$res2" | bc -l` ))
 then
-	max=$res2;
-elif ((res1<res3))
+	max=`echo "$res2" | bc -l`;
+elif (( `echo "$res1<$res3" | bc -l` ))
 then 
-	max=$res3;
-elif ((res1<res4))
+	max=`echo "$res3" | bc -l`;
+elif (( `echo "$res1<$res4" | bc -l` ))
 then 
-        max=$res4;
+        max=`echo "$res4" | bc -l`;
 fi
 echo "Maximum value is:"$max ;
 
-if ((res1>res2))
+if (( `echo "$res1>$res2" | bc -l` ))
 then
-	min=$res2;
-elif ((res1>res3))
+	min=`echo "$res2" | bc -l`;
+elif (( `echo "$res1>$res3" | bc -l` ))
 then 
-        min=$res3;
+        min=`echo "$res3" | bc -l`;
 
-elif ((res1>res4))
+elif (( `echo "$res1>$res4" | bc -l` ))
 then 
-        min=$res4;
+        min=`echo "$res4" | bc -l`;
 
 fi
 echo "Minimum value is :"$min;
