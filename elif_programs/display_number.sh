486dce237d5aeb6bd2fee7a6f3f1bfe667b654be
Imran01000 <imshaik01000@gmail.com>
2020-02-19 11:26:10 +0530

    by else-if

--- elif_programs/display_number.sh
+++ elif_programs/display_number.sh
@@ -1,18 +1,65 @@
 read -p "Enter the number " n
-if ((n==1))
+if ((${#n}==4))
 then
-	echo "unit";
-elif ((n==10))
-then 
-	echo "ten";
-elif ((n=100))
-then 
-        echo "hundred";
-elif ((n==1000))
-then 
-        echo "thousand";
-elif ((n==10000))
-then 
-        echo "ten thousand";
+ 	n1=$(($n%10));
+	echo $n1 "Is in unit place!";
+	n=$(($n/10));
+
+	if ((${#n}==3))
+	then 
+		n1=$(($n%10));
+		echo  $n1 "Is in ten place!";
+		n=$(($n/10));
+	fi
+	if ((${#n}==2))
+	then 
+                n1=$(($n%10));
+        	echo  $n1 "Is in hundred place!";
+		n=$(($n/10));
+	fi
+	if ((${#n}==1))
+	then 
+        	n1=$(($n%10));
+		echo  $n1 "Is in thousand place!";
+	fi
+
+elif ((${#n}==3))
+then
+        n1=$(($n%10));
+        echo $n1 "Is in unit place!";
+        n=$(($n/10));
+
+        if ((${#n}==2))
+        then 
+                n1=$(($n%10));
+                echo  $n1 "Is in ten place!";
+                n=$(($n/10));
+        fi
+        if ((${#n}==1))
+        then 
+                n1=$(($n%10));
+                echo  $n1 "Is in hundred place!";
+                n=$(($n/10));
+         fi
+		 
+elif ((${#n}==2))
+then
+	n1=$(($n%10));
+        echo $n1 "Is in unit place!";
+        n=$(($n/10));
+
+        if ((${#n}==1))
+        then 
+                n1=$(($n%10));
+                echo  $n1 "Is in ten place!";
+                n=$(($n/10));
+        fi
+elif ((${#n}==1))
+then
+	n1=$(($n%10));
+        echo $n1 "Is in unit place!";
+        n=$(($n/10));
 fi
+
+
 
